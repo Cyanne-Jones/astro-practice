ff08900802c02a7239ebb76c92d5353a49c37b2c
Cyanne Jones <jonescyanne@yahoo.com>
2026-02-20 17:18:29 -0700

    add some interactivity

--- interactive-island-in-the-sun/src/scripts/menu.ts
+++ interactive-island-in-the-sun/src/scripts/menu.ts
@@ -3,4 +3,5 @@ const menu = document.querySelector('.menu');
 menu?.addEventListener('click', () => {
   const isExpanded = menu.getAttribute('aria-expanded') === 'true';
   menu.setAttribute('aria-expanded', `${!isExpanded}`);
+  menu.innerHTML = isExpanded ? '♥︎' : 'X';
 });
